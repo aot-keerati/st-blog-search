bd35708bc09e9ee6e8b29f895c785e0d0977417b
Aot Keerati <136554245+aot-keerati@users.noreply.github.com>
2023-12-14 23:50:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ import openai
 import pinecone
 import streamlit as st
 
-
 PINECONE_API_KEY = '0a310604-6e64-4616-b258-a049bec92e82'
 PINECONE_ENV = 'gcp-starter'
 COHERE_API_KEY = 'kpZfBhHE4FSzvdZeSE2SK50oblBd7kHYVLqZlGeJ'
 
-openai.api_key = OPENAI_API_KEY
 pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
 co = cohere.Client(COHERE_API_KEY)
 INDEX_NAME = 'blog-index'
@@ -38,14 +36,9 @@ PROMPT = f.read()
 f.close()
 
 def articles_search(query, k):
-    vector_found = [i for i in keep_vectors if i.get('query') == query]
-    if len(vector_found) == 1: # case vector exist
-        query_vector = vector_found[0]['values']
-    else : # case vector doesn't exist
-        query_vector = co.embed(texts = [query],
+    query_vector = co.embed(texts = [query],
                                 model='embed-multilingual-v2.0',
                                 input_type='search_query').embeddings
-        keep_vectors.append({'query': query, 'values': query_vector})
 
     search_response = index.query(
         top_k = k,
@@ -74,17 +67,31 @@ st.set_page_config(page_title="Blog search")
 st.write("## Blog search")
 st.caption("เว็บแอปพลิเคชั่นระบบสืบค้นข้อมูล ที่ค้นหาข้อมูลด้วย vectorized search และสร้างคำตอบด้วย GPT-3.5 model")
 
+OPENAI_API_KEY = st.sidebar.text_input("กรอก OpenAI API Key:", type="password")
+
 with st.form('form_1'):
   query = st.text_area('พิมพ์คำถามหรือคีย์เวิร์ดที่ต้องการค้นหา:', placeholder='เช่น ปลิงทะเลชมพู, พลังงานชีวภาพ คืออะไร')
   search_btn = st.form_submit_button('Submit')
-  if search_btn:
-    articles = articles_search(query, k=4)
-    res = qa(query, articles)
-
-    st.write('### Answer:')
-    st.write(res.choices[0].message.content)
-    st.write('บทความที่เกี่ยวข้องมากที่สุด ได้แก่:')
-    st.markdown(create_hyperlink(articles[0]['metadata']['url'], articles[0]['metadata']['title']), unsafe_allow_html=True)
-
-    st.caption(res.usage)
-    st.write(articles)
+  if search_btn, query:
+      if not OPENAI_API_KEY:
+        st.info("Please add your OpenAI API key to continue.")
+      else:
+        openai.api_key = OPENAI_API_KEY
+        
+        articles = articles_search(query, k=4)
+        res = response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{ "role": "user", "content": PROMPT.format(
+                    related_articles = articles,
+                    user_query = query) }
+            ],
+            temperature=0, max_tokens=500
+        )
+    
+        st.write('### Answer:')
+        st.write(res.choices[0].message.content)
+        st.write('บทความที่เกี่ยวข้องมากที่สุด ได้แก่:')
+        st.markdown(create_hyperlink(articles[0]['metadata']['url'], articles[0]['metadata']['title']), unsafe_allow_html=True)
+    
+        st.caption(res.usage)
+        st.write(articles)
